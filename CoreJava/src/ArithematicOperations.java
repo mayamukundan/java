9c58e13394a439e49d2937c1b7c74a2a4ccc78ff
Jayram Rout <jayram.rout@gmail.com>
2019-06-05 22:16:09 -0400

    Update

--- CoreJava/src/ArithematicOperations.java
+++ CoreJava/src/ArithematicOperations.java
@@ -6,11 +6,32 @@ public class ArithematicOperations {
 
 //        String output = 3+5+4+6 + "Is the Final Answer : ";
 
-        System.out.println(7-3);
+//        System.out.println(7-3);
+//
+//        System.out.println(5%2); // the reminder...
+        //
+//        12
+//        8
+        int age =  10;
 
-        System.out.println(5%2); // the reminder...
+        age++; // 11
+        age++; // 12
+        System.out.println(age++); // 13// this print the actual value and then increments; POST Increment
+//        System.out.println(++age); // this print the actual value and then increments; PRE Increment
+        age--; // 12
+        age--; // 11
+        age--; // 10
+        age--; // 9
+        System.out.println(age);
+
+        int year = 2000;
+//        year =  year + 2;
+        year *= 2;
+        System.out.println("Year is :"+ year);
 
 
+//        int height = 3;
+//        System.out.println("Height ="+ ++height);
 
 
     }
