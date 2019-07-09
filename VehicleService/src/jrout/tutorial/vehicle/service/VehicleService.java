892b57b6f1b305132a5a02a28a3334ca3c2614d5
Jayram Rout <jayram.rout@gmail.com>
2019-07-08 22:24:31 -0400

    Updates

---
+++ VehicleService/src/jrout/tutorial/vehicle/service/VehicleService.java
@@ -0,0 +1,17 @@
+package jrout.tutorial.vehicle.service;
+
+/**
+ * Created by jrout on 7/8/19.
+ */
+public class VehicleService {
+
+    public void doService(String vehicleType){
+        if(vehicleType.equalsIgnoreCase("honda")){
+            System.out.println("First clean the Tyre");
+            System.out.println("Clean the Engine...");
+        }else if(vehicleType.equalsIgnoreCase("nissan")){
+            System.out.println("Clean the Engine...");
+            System.out.println("Check air presssure");
+        }
+    }
+}
